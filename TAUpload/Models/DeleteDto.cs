8b5ac3d487439a3b09625cef759fd2d70c64a01e
egor-geller <egkingers11@gmail.com>
2023-08-08 11:58:05 +0300

    Delete Dto

---
+++ TAUpload/Models/DeleteDto.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace TAUpload.Models
+{
+    public sealed class DeleteDto
+    {
+        [Required]
+        public string? DbId { get; set; }
+        [Required]
+        public string? SqlServerName { get; set; }
+        [Required]
+        public string? SqlDbName { get; set; }
+        [Required]
+        public string? ObjectType { get; set; }
+        [Required]
+        public string PathName { get; set; } = string.Empty;
+        [Required]
+        public string? FileName { get; set; }
+
+        public override string? ToString()
+        {
+            return new StringBuilder()
+                .Append("DbId: ").Append(DbId).Append('\n')
+                .Append("ObjectType: ").Append(ObjectType).Append('\n')
+                .Append("SqlDbName: ").Append(SqlDbName).Append('\n')
+                .Append("SqlServerName: ").Append(SqlServerName).Append('\n')
+                .Append("FileName: ").Append(FileName).Append('\n')
+                .Append("PathName: ").Append(PathName).Append('\n')
+                .ToString();
+        }
+    }
+}
